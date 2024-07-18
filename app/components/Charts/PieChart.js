e50a043c1a3a02d6eaa1a489d0675098698352af
Priyosmita <daspriyosmita2003@gmail.com>
2024-07-18 12:02:21 +0530

    hehe

---
+++ app/components/Charts/PieChart.js
@@ -0,0 +1,31 @@
+// PieChart.js
+import React from 'react';
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+// Register the required components
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+// Custom plugin for the glow effect
+const glowPlugin = {
+  id: 'glow',
+  beforeDraw: function (chart) {
+    const ctx = chart.ctx;
+    ctx.save();
+    ctx.filter = 'drop-shadow(10px 10px 10px rgba(255, 99, 132, 0.8))'; // Adjust the shadow properties as needed
+    chart.update();
+  },
+  afterDraw: function (chart) {
+    chart.ctx.restore();
+  }
+};
+
+const PieChart = ({ data, options }) => {
+  return (
+    <div className="pie-chart-container" style={{ position: 'relative', height: '400px', width: '400px' }}>
+      <Pie data={data} options={options} plugins={[glowPlugin]} />
+    </div>
+  );
+};
+
+export default PieChart;
