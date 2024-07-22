848e375aa15294a69c2c8f38a0e1b070c8c9b73b
Priyosmita <daspriyosmita2003@gmail.com>
2024-07-23 01:45:14 +0530

    hello2

---
+++ .next/server/vendor-chunks/isomorphic-ws.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/isomorphic-ws";
+exports.ids = ["vendor-chunks/isomorphic-ws"];
+exports.modules = {
+
+/***/ "(ssr)/./node_modules/isomorphic-ws/node.js":
+/*!********************************************!*\
+  !*** ./node_modules/isomorphic-ws/node.js ***!
+  \********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("\n\nmodule.exports = __webpack_require__(/*! ws */ \"(ssr)/./node_modules/ws/index.js\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHNzcikvLi9ub2RlX21vZHVsZXMvaXNvbW9ycGhpYy13cy9ub2RlLmpzIiwibWFwcGluZ3MiOiJBQUFhOztBQUViLGtGQUE4QiIsInNvdXJjZXMiOlsid2VicGFjazovL2Zyb250ZW5kLy4vbm9kZV9tb2R1bGVzL2lzb21vcnBoaWMtd3Mvbm9kZS5qcz9mNzM0Il0sInNvdXJjZXNDb250ZW50IjpbIlwidXNlIHN0cmljdFwiO1xuXG5tb2R1bGUuZXhwb3J0cyA9IHJlcXVpcmUoJ3dzJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(ssr)/./node_modules/isomorphic-ws/node.js\n");
+
+/***/ })
+
+};
+;
